ca84cb041719ff094c6cb4efa91de807ed2578d0
Lilith Lamber <winter.rituel@gmail.com>
2022-02-20 16:40:45 +0200

    small chart fixes

--- src/components/Sparkline/Chart/index.jsx
+++ src/components/Sparkline/Chart/index.jsx
@@ -10,17 +10,17 @@ export default function Chart({selection, colorKey}) {
                 const date = moment()
                     .subtract(timeToSubtract, "hours")
                     .format("ddd h:mma");
-                return { value: price.toFixed(4), date };
+                return { price: price.toFixed(4), date };
             } else if (idx === selection.length - 1) {
                 const date = moment().format("ddd h:mma");
-                return { value: price.toFixed(4), date };
+                return { price: price.toFixed(4), date };
             }
             return null;
         })
         .filter(data => data);
     return (
         <ResponsiveContainer width={"100%"} height={"100%"}>
-            <LineChart width={1100} height={300} data={formattedData}  margin={{ top: 5, right: 0, left: -30, bottom: 0 }}>
+            <LineChart width={1100} height={300} data={formattedData}  margin={{ top: 10, right: 0, left: -30, bottom: 0 }}>
                 <defs>
                     <linearGradient id="btc"
                                     x1="0"
@@ -71,9 +71,9 @@ export default function Chart({selection, colorKey}) {
                         <stop offset="100%" stopColor="#00d4ff" />
                     </linearGradient>
                 </defs>
-                <Line type="monotone" dataKey="value" stroke={`url(#${colorKey})`} strokeWidth="5px" dot={false} />
+                <Line type="monotone" dataKey="price" stroke={`url(#${colorKey})`} strokeWidth="5px" dot={false} />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <XAxis dataKey="date" interval={5}   />
+                <XAxis dataKey="date" interval={4}   />
                 <YAxis tickFormatter={tick => {return kFormatter(tick)}} />
                 <Tooltip wrapperClassName="recharts-tooltip" labelClassName="recharts-tooltip-label" />
             </LineChart>
